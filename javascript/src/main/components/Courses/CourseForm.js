75d56373eb6afb2ded3d823f7bcd4d035322d923
Phillip Conrad <pconrad@cs.ucsb.edu>
2020-11-16 14:13:56 -0800

    pc - complete test coverage of CourseForm.js

--- javascript/src/main/components/Courses/CourseForm.js
+++ javascript/src/main/components/Courses/CourseForm.js
@@ -16,16 +16,14 @@ const CourseForm = ({ createCourse, updateCourse, existingCourse }) => {
         e.preventDefault();
         if (createCourse)
             createCourse(course.name, course.quarter, course.instructorFirstName, course.instructorLastName, course.instructorEmail);
-        else if (updateCourse)
+        else 
             updateCourse(course, course.id);
     }
 
     return (
         <Form onSubmit={handleOnSubmit}>
-            <Form.Group as={Row}>
-                <Form.Label column sm={2}>
-                    Course Name
-                </Form.Label>
+            <Form.Group as={Row} controlId="name">
+                <Form.Label column sm={2}>Course Name</Form.Label>
                 <Col sm={10}>
                     <Form.Control type="text" placeholder="course name" value={course.name} onChange={(e) => setCourse({
                         ...course,
@@ -33,7 +31,7 @@ const CourseForm = ({ createCourse, updateCourse, existingCourse }) => {
                     })} />
                 </Col>
             </Form.Group>
-            <Form.Group as={Row}>
+            <Form.Group as={Row} controlId="quarter">
                 <Form.Label column sm={2}>
                     Quarter
                 </Form.Label>
@@ -44,10 +42,8 @@ const CourseForm = ({ createCourse, updateCourse, existingCourse }) => {
                     })} />
                 </Col>
             </Form.Group>
-            <Form.Group as={Row}>
-                <Form.Label column sm={2}>
-                    Last Name
-                </Form.Label>
+            <Form.Group as={Row} controlId="firstName">
+                <Form.Label column sm={2}>First Name</Form.Label>
                 <Col sm={10}>
                     <Form.Control type="text" placeholder="instructor first name" value={course.instructorFirstName} onChange={(e) => setCourse({
                         ...course,
@@ -55,9 +51,9 @@ const CourseForm = ({ createCourse, updateCourse, existingCourse }) => {
                     })} />
                 </Col>
             </Form.Group>
-            <Form.Group as={Row}>
+            <Form.Group as={Row} controlId="lastName">
                 <Form.Label column sm={2}>
-                    First Name
+                    Last Name
                 </Form.Label>
                 <Col sm={10}>
                     <Form.Control type="text" placeholder="instructor last name" value={course.instructorLastName} onChange={(e) => setCourse({
@@ -66,7 +62,7 @@ const CourseForm = ({ createCourse, updateCourse, existingCourse }) => {
                     })} />
                 </Col>
             </Form.Group>
-            <Form.Group as={Row}>
+            <Form.Group as={Row} controlId="email">
                 <Form.Label column sm={2}>
                     Email
                 </Form.Label>
--- javascript/src/test/components/Courses/CourseForm.test.js
+++ javascript/src/test/components/Courses/CourseForm.test.js
@@ -1,10 +1,87 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import CourseForm from "main/components/Footer/AppFooter";
-
+import { render, waitFor } from "@testing-library/react";
+import CourseForm from "main/components/Courses/CourseForm";
+import userEvent from "@testing-library/user-event";
 
 describe("CourseForm tests", () => {
-  test("renders without crashing", () => {
+
+  const sampleCourse = {
+    "id": 1,
+    "name": "CMPSC 156",
+    "quarter": "F20",
+    "instructorFirstName": "Phill",
+    "instructorLastName": "Conrad",
+    "instructorEmail": "phtcon@ucsb.edu"
+  }
+
+  test("empty component renders without crashing", () => {
     render(<CourseForm />);
   });
+
+  test("component with existing course renders without crashing", () => {
+    render(<CourseForm existingCourse={sampleCourse}/>);
+  });
+
+  test("updating course name works", async () => {
+
+    const updateCourseMock = jest.fn();
+
+    const { getByText, getByDisplayValue } = render
+      (<CourseForm updateCourse={updateCourseMock} existingCourse={sampleCourse}/>)
+    ;
+
+    const updatedCourse = {
+      ...sampleCourse,
+      name: "updated name",
+    };
+
+    const input = getByDisplayValue(sampleCourse.name);
+    userEvent.clear(input);
+    userEvent.type(input, updatedCourse.name);
+
+    const submitButton = getByText("Submit");
+    userEvent.click(submitButton);
+
+    expect(updateCourseMock).toHaveBeenCalledTimes(1);
+    expect(updateCourseMock).toHaveBeenCalledWith(updatedCourse, updatedCourse.id);
+
+  });
+
+  test("updating creating course works", async () => {
+
+    const createCourseMock = jest.fn();
+
+    const { getByLabelText, getByText } = render
+      (<CourseForm createCourse={createCourseMock} />)
+    ;
+
+    const nameInput = getByLabelText("Course Name");
+    userEvent.type(nameInput, sampleCourse.name);
+
+    const quarterInput = getByLabelText("Quarter");
+    userEvent.type(quarterInput, sampleCourse.quarter);
+
+    const fnameInput = getByLabelText("First Name");
+    userEvent.type(fnameInput, sampleCourse.instructorFirstName);
+
+    const lnameInput = getByLabelText("Last Name");
+    userEvent.type(lnameInput, sampleCourse.instructorLastName);
+
+    const emailInput = getByLabelText("Email");
+    userEvent.type(emailInput, sampleCourse.instructorEmail);
+
+    const submitButton = getByText("Submit");
+    userEvent.click(submitButton);
+
+    expect(createCourseMock).toHaveBeenCalledTimes(1);
+    expect(createCourseMock).toHaveBeenCalledWith(
+      sampleCourse.name, 
+      sampleCourse.quarter, 
+      sampleCourse.instructorFirstName, 
+      sampleCourse.instructorLastName, 
+      sampleCourse.instructorEmail, 
+      );
+
+  });
+
 });
