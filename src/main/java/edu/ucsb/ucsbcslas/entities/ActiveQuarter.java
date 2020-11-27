3f0255ba1cf28aa7fceafc0960975569bb5c1177
KoskiTravis <tmkoski69@gmail.com>
2020-11-26 19:24:06 -0800

    6pm-b-tk added entity which represents the value of the Active quarter to filter by. This has one piece of data (the quarter itself) which will be stored in db for frontend to use

---
+++ src/main/java/edu/ucsb/ucsbcslas/entities/ActiveQuarter.java
@@ -0,0 +1,61 @@
+package edu.ucsb.ucsbcslas.entities;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
+@Entity
+public class ActiveQuarter {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof ActiveQuarter)) {
+            return false;
+        }
+        ActiveQuarter activeQuarter = (ActiveQuarter) o;
+        EqualsBuilder e = new EqualsBuilder();
+        e.append(activeQuarter, other.activeQuarter);
+        return e.isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, activeQuarterValue);
+    }
+
+  @Column(nullable = false)
+  private String activeQuarterValue;
+
+    public ActiveQuarter(String activeQuarterValue) {
+        this.activeQuarterValue = activeQuarterValue;
+    }
+    public ActiveQuarter(){
+
+    }
+
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getActiveQuarterValue() {
+        return this.activeQuarterValue;
+    }
+
+    public void setActiveQuarterValue(String activeQuarterValue) {
+        this.activeQuarterValue = activeQuarterValue;
+    }
+}
