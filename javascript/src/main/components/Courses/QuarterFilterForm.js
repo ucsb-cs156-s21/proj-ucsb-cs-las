5b68d2564690c98add3206e840f88b70f9eb590a
KoskiTravis <tmkoski69@gmail.com>
2020-11-26 21:38:22 -0800

    6pm-b-tk the user can now type into the form and hit a submit button, the backend calling functions still need implementations

--- javascript/src/main/components/Courses/QuarterFilterForm.js
+++ javascript/src/main/components/Courses/QuarterFilterForm.js
@@ -1,13 +1,33 @@
 import React, { useState } from "react";
 import { Form, Button, Row, Col, Container } from "react-bootstrap";
 
-const QuarterFilterForm = () => {
+const QuarterFilterForm = (existingFilter, createFilter, updateFilter, deleteFilter) => {
+    const emptyFilter = {
+        "quarterFilterValue": ""
+    }
+    const [filter, setFilter] = useState(emptyFilter || existingFilter);
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        if (createFilter) {
+            createFilter(filter);
+        }
+        else if (updateFilter) {
+            updateFilter(filter);
+        }
+        else {
+            deleteFilter();
+        }
+
+    }
     return (
-        <Form>
+        <Form onSubmit={handleOnSubmit}>
             <Form.Group as={Row} controlId="name">
                 <Form.Label column sm={2}>Enter quarter to filter by</Form.Label>
                 <Col sm={10}>
-                    <Form.Control type="text" placeholder="active quarter to filter by"
+                    <Form.Control type="text" placeholder="active quarter to filter by" value={filter.quarterFilterValue} onChange={
+                        (e) => setFilter({ ...filter, quarterFilterValue: e.target.value })
+                    }
                     />
 
                 </Col>
@@ -17,6 +37,13 @@ const QuarterFilterForm = () => {
                     <Button type="submit">Submit</Button>
                 </Col>
             </Form.Group>
+            {existingFilter ?
+                <Form.Group as={Row}>
+                    <Col sm={{ span: 10, offset: 2 }}>
+                        <Button type="submit">Submit</Button>
+                    </Col>
+                </Form.Group> : <></>}
+
         </Form>
     );
 };
