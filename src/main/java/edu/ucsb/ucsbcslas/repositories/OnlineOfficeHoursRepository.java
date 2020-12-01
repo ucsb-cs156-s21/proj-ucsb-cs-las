4a004718d70a251a22be5ba933e96fa73f3e0d24
kourosh-safari <kouroshsafari@ucsb.edu>
2020-12-01 14:56:23 -0800

    ks Added a repository file for online office hours

---
+++ src/main/java/edu/ucsb/ucsbcslas/repositories/OnlineOfficeHoursRepository.java
@@ -0,0 +1,11 @@
+package edu.ucsb.ucsbcslas.repositories;
+
+import edu.ucsb.ucsbcslas.entities.OnlineOfficeHours;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OnlineOfficeHoursRepository extends CrudRepository<OnlineOfficeHours, Long> {
+    public List<OnlineOfficeHours> findAll();
+}
