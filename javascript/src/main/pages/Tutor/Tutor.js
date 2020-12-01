f55ca33c23b0409a44201ee9ebfc0fb9ee166084
long hoang <longhoang@ucsb.edu>
2020-12-01 15:04:21 -0800

    gm/lh finished Tutoe.js page

---
+++ javascript/src/main/pages/Tutor/Tutor.js
@@ -0,0 +1,38 @@
+import React from "react";
+import useSWR from "swr";
+import { Button } from "react-bootstrap";
+import { fetchWithToken } from "main/utils/fetch";
+import { useAuth0 } from "@auth0/auth0-react";
+import Loading from "main/components/Loading/Loading";
+import TutorTable from "main/components/Tutor/TutorTable"
+import { buildCreateTutor, buildDeleteTutor, buildUpdateTutor } from "main/services/Tutor/TutorService";
+
+import {useHistory} from "react-router-dom";
+
+
+const Tutor = () => {
+  const history = useHistory();
+  const { getAccessTokenSilently: getToken } = useAuth0();
+  const { data: tutorList, error, mutate: mutateTutors } = useSWR(
+    ["/api/public/tutors", getToken],
+    fetchWithToken
+  );
+  if (error) {
+    return (
+      <h1>We encountered an error; please reload the page and try again.</h1>
+    );
+  }
+  if (!tutorList) {
+    return <Loading />;
+  }
+  const deleteTutor = buildDeleteTutor(getToken, mutateTutors);
+
+  return (
+    <>
+      <Button onClick={()=>history.push("/tutors/new")}>New Tutor</Button>
+      <TutorTable tutors={tutorList} admin={true} deleteTutor={deleteTutor} />
+    </>
+  );
+};
+
+export default Tutor;
