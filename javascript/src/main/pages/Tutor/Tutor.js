58bfd42a4bc3b484b0bdf5072ede518591e09e74
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-12-03 15:38:44 -0800

    gm/lh - perform api call to retrieve roll. Conditionally display new role button

--- javascript/src/main/pages/Tutor/Tutor.js
+++ javascript/src/main/pages/Tutor/Tutor.js
@@ -4,19 +4,24 @@ import { Button } from "react-bootstrap";
 import { fetchWithToken } from "main/utils/fetch";
 import { useAuth0 } from "@auth0/auth0-react";
 import Loading from "main/components/Loading/Loading";
-import TutorTable from "main/components/Tutor/TutorTable"
-import { buildCreateTutor, buildDeleteTutor, buildUpdateTutor } from "main/services/Tutor/TutorService";
+import TutorTable from "main/components/Tutor/TutorTable";
+import {
+  buildCreateTutor,
+  buildDeleteTutor,
+  buildUpdateTutor
+} from "main/services/Tutor/TutorService";
 
-import {useHistory} from "react-router-dom";
-
+import { useHistory } from "react-router-dom";
 
 const Tutor = () => {
   const history = useHistory();
   const { getAccessTokenSilently: getToken } = useAuth0();
+  const { data: roleInfo } = useSWR(["/api/myRole", getToken], fetchWithToken);
   const { data: tutorList, error, mutate: mutateTutors } = useSWR(
     ["/api/public/tutors", getToken],
     fetchWithToken
   );
+
   if (error) {
     return (
       <h1>We encountered an error; please reload the page and try again.</h1>
@@ -26,11 +31,18 @@ const Tutor = () => {
     return <Loading />;
   }
   const deleteTutor = buildDeleteTutor(getToken, mutateTutors);
+  const isAdmin = roleInfo && roleInfo.role.toLowerCase() == "admin";
 
   return (
     <>
-      <Button onClick={()=>history.push("/tutors/new")}>New Tutor</Button>
-      <TutorTable tutors={tutorList} admin={true} deleteTutor={deleteTutor} />
+      {isAdmin && (
+        <Button onClick={() => history.push("/tutors/new")}>New Tutor</Button>
+      )}
+      <TutorTable
+        tutors={tutorList}
+        admin={isAdmin}
+        deleteTutor={deleteTutor}
+      />
     </>
   );
 };
