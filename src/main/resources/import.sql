b7d0ed53373c815810e262fd79a0e27e796d0880
Jiawei Yu <jiaweiyu2009@gmail.com>
2020-12-08 12:29:13 -0800

    jy - added sql data to test

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO course(id, name, quarter, instructor_first_name, instructor_last_name, instructor_email) VALUES ('1', 'CMPSC 156', '20204', 'Phill', 'Conrad', 'phtcon@ucsb.edu');
+INSERT INTO course(id, name, quarter, instructor_first_name, instructor_last_name, instructor_email) VALUES ('2', 'CMPSC 148', '20204', 'Chandra', 'Krintz', 'ckrintz@ucsb.edu');
+
