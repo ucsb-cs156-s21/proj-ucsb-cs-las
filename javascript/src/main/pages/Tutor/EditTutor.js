2831e41035395805bc1041d8e2f39ec943e10238
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-12-07 21:12:06 -0800

    gm - changed api endpoint path to /admin from /public

--- javascript/src/main/pages/Tutor/EditTutor.js
+++ javascript/src/main/pages/Tutor/EditTutor.js
@@ -6,8 +6,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Loading from "main/components/Loading/Loading";
 import { useParams, useHistory } from "react-router-dom";
 import { buildUpdateTutor } from "main/services/Tutor/TutorService";
-import { useToasts } from 'react-toast-notifications'
-
+import { useToasts } from "react-toast-notifications";
 
 const EditTutor = () => {
   const { addToast } = useToasts();
@@ -15,30 +14,30 @@ const EditTutor = () => {
   const { tutorId } = useParams();
   const { getAccessTokenSilently: getToken } = useAuth0();
   const updateTutor = buildUpdateTutor(
-    getToken, 
-    () => { 
-      history.push("/tutors"); 
-      addToast("Tutor updated", { appearance: 'success' });
-    }, 
-    () => { 
-      addToast("Error saving tutor", { appearance: 'error' });
+    getToken,
+    () => {
+      history.push("/tutors");
+      addToast("Tutor updated", { appearance: "success" });
+    },
+    () => {
+      addToast("Error saving tutor", { appearance: "error" });
     }
   );
 
   cache.clear();
   const { data: tutor } = useSWR(
-    [`/api/public/tutors/${tutorId}`, getToken],
+    [`/api/member/tutors/${tutorId}`, getToken],
     fetchWithToken
   );
 
   return (
     <>
       <h1>Edit Tutor</h1>
-      {
-        tutor ?
-          <TutorForm updateTutor={updateTutor} existingTutor={tutor} /> :
-          <Loading />
-      }
+      {tutor ? (
+        <TutorForm updateTutor={updateTutor} existingTutor={tutor} />
+      ) : (
+        <Loading />
+      )}
     </>
   );
 };
