4748c9dfa0ed1f776a457136575dcc8d7b4908bb
William Mori <wmori@ucsb.edu>
2020-12-09 12:40:24 -0800

    merge conflict resolved

---
+++ javascript/src/test/components/Courses/CourseTable.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import CourseTable from "main/components/Courses/CourseTable"
+import userEvent from "@testing-library/user-event";
+
+
+describe("CourseForm tests", () => {
+
+  const courseList = [];
+  const deleteCourse = jest.fn();
+  test("renders without crashing", () => {
+    render(<CourseTable courses={courseList} admin={true} deleteCourse={deleteCourse} />);
+  });
+
+  test("ascending and descending buttons can be pressed", () => {
+    const { getByText } = render(<CourseTable courses={courseList} admin={true} deleteCourse={deleteCourse} />);
+    const courseNumberHeader = getByText(/Course Number/);
+
+    //cycle through both ascending and descending for test coverage
+    userEvent.click(courseNumberHeader);
+    userEvent.click(courseNumberHeader);
+
+  });
+
+});
