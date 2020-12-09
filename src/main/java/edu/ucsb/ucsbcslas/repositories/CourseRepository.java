739b850641100cf717c238e4dc30de8c36e53de5
KoskiTravis <tmkoski69@gmail.com>
2020-12-09 09:22:49 -0800

    resolved merge conflict in courserepository

--- src/main/java/edu/ucsb/ucsbcslas/repositories/CourseRepository.java
+++ src/main/java/edu/ucsb/ucsbcslas/repositories/CourseRepository.java
@@ -10,5 +10,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CourseRepository extends CrudRepository<Course, Long> {
   List<Course> findAll();
-  List<Course> findByQuarter(String activeQuarter);
 }
