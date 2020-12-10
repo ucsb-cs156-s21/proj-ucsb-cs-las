135d0f7326dae64baff6b467ca702ebc550a3ad6
Luke Lewis <lukelewis@ucsb.edu>
2020-12-10 14:17:44 -0800

    ll - reverted typo in AppFooter back from abcd to a

--- javascript/src/main/components/Footer/AppFooter.js
+++ javascript/src/main/components/Footer/AppFooter.js
@@ -3,7 +3,7 @@ import React from "react";
 const AppFooter = () => {
   return (
     <footer className="bg-light p-3 text-center">
-      This is abcd demo of a minimal React frontend + Spring Boot backend app.
+      This is a demo of a minimal React frontend + Spring Boot backend app.
       Check out the source code on{" "}
       <a href="https://github.com/ucsb-cs156-f20/demo-spring-nextjs-app">
         GitHub
