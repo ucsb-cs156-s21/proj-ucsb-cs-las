9a8ebdea658696439fa32ec808cbc545f94b4564
Jbo <zhenglin@ucsb.edu>
2020-12-10 10:34:57 +0800

    Add files via upload

---
+++ src/main/java/edu/ucsb/ucsbcslas/repositories/CourseController.java
@@ -0,0 +1,231 @@
+package edu.ucsb.ucsbcslas.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Optional;
+import javax.validation.Valid;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.ucsb.ucsbcslas.advice.AuthControllerAdvice;
+import edu.ucsb.ucsbcslas.entities.OnlineOfficeHours;
+import edu.ucsb.ucsbcslas.entities.TutorAssignment;
+import edu.ucsb.ucsbcslas.entities.Tutor;
+import edu.ucsb.ucsbcslas.models.Course;
+import edu.ucsb.ucsbcslas.models.TutorAssignmentOfficeHourView;
+import edu.ucsb.ucsbcslas.repositories.CourseRepository;
+import edu.ucsb.ucsbcslas.repositories.OnlineOfficeHoursRepository;
+import edu.ucsb.ucsbcslas.repositories.TutorAssignmentRepository;
+import edu.ucsb.ucsbcslas.repositories.TutorRepository;
+
+
+@RestController
+public class CourseController {
+  private final Logger logger = LoggerFactory.getLogger(CourseController.class);
+
+  @Autowired
+  private AuthControllerAdvice authControllerAdvice;
+  @Autowired
+  private CourseRepository courseRepository;
+  @Autowired
+  private TutorAssignmentRepository tutorAssignmentRepository;
+  @Autowired
+  private OnlineOfficeHoursRepository onlineOfficeHoursRespository;
+
+  private ObjectMapper mapper = new ObjectMapper();
+
+  private ResponseEntity<String> getUnauthorizedResponse(String roleRequired) throws JsonProcessingException {
+    Map<String, String> response = new HashMap<String, String>();
+    response.put("error", String.format("Unauthorized; only %s may access this resource.", roleRequired));
+    String body = mapper.writeValueAsString(response);
+    return new ResponseEntity<String>(body, HttpStatus.UNAUTHORIZED);
+  }
+
+  @PostMapping(value = "/api/admin/courses", produces = "application/json")
+  public ResponseEntity<String> createCourse(@RequestHeader("Authorization") String authorization,
+      @RequestBody @Valid Course course) throws JsonProcessingException {
+    if (!authControllerAdvice.getIsAdmin(authorization))
+      return getUnauthorizedResponse("admin");
+    Course savedCourse = courseRepository.save(course);
+    String body = mapper.writeValueAsString(savedCourse);
+    return ResponseEntity.ok().body(body);
+  }
+
+  @PutMapping(value = "/api/admin/courses/{id}", produces = "application/json")
+  public ResponseEntity<String> updateCourse(@RequestHeader("Authorization") String authorization,
+      @PathVariable("id") Long id, @RequestBody @Valid Course incomingCourse) throws JsonProcessingException {
+    if (!authControllerAdvice.getIsAdmin(authorization))
+      return getUnauthorizedResponse("admin");
+    Optional<Course> course = courseRepository.findById(id);
+    if (!course.isPresent()) {
+      return ResponseEntity.notFound().build();
+    }
+
+    if (!incomingCourse.getId().equals(id)) {
+      return ResponseEntity.badRequest().build();
+    }
+
+    courseRepository.save(incomingCourse);
+    String body = mapper.writeValueAsString(incomingCourse);
+    return ResponseEntity.ok().body(body);
+  }
+
+  @DeleteMapping(value = "/api/admin/courses/{id}", produces = "application/json")
+  public ResponseEntity<String> deleteCourse(@RequestHeader("Authorization") String authorization,
+      @PathVariable("id") Long id) throws JsonProcessingException {
+    if (!authControllerAdvice.getIsAdmin(authorization))
+      return getUnauthorizedResponse("admin");
+    Optional<Course> course = courseRepository.findById(id);
+    if (!course.isPresent()) {
+      return ResponseEntity.notFound().build();
+    }
+    courseRepository.deleteById(id);
+    return ResponseEntity.noContent().build();
+  }
+
+  @GetMapping(value = "/api/public/courses", produces = "application/json")
+  public ResponseEntity<String> getCourses() throws JsonProcessingException {
+    List<Course> courseList = courseRepository.findAll();
+    ObjectMapper mapper = new ObjectMapper();
+
+    String body = mapper.writeValueAsString(courseList);
+    return ResponseEntity.ok().body(body);
+  }
+
+  @GetMapping(value = "/api/public/courses/{id}", produces = "application/json")
+  public ResponseEntity<String> getCourse(@PathVariable("id") Long id) throws JsonProcessingException {
+    Optional<Course> course = courseRepository.findById(id);
+    if (course.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+
+    ObjectMapper mapper = new ObjectMapper();
+    String body = mapper.writeValueAsString(course.get());
+    return ResponseEntity.ok().body(body);
+  }
+
+  @GetMapping(value = "/api/member/courses", produces = "application/json")
+  public ResponseEntity<String> getMyCourses(@RequestHeader("Authorization") String authorization) throws JsonProcessingException {
+    if (authControllerAdvice.getIsAdmin(authorization)){
+      List<Course> courseList = courseRepository.findAll();
+      if(courseList.isEmpty()){
+        return ResponseEntity.notFound().build();
+      }
+      ObjectMapper mapper = new ObjectMapper();
+      String body = mapper.writeValueAsString(courseList);
+      return ResponseEntity.ok().body(body);
+    } 
+    else {
+      List<Course> courseList = courseRepository.findAllByInstructorEmail(authControllerAdvice.getUser(authorization).getEmail());
+      if(courseList.isEmpty()){
+        return getUnauthorizedResponse("instructor");
+      }
+      ObjectMapper mapper = new ObjectMapper();
+      String body = mapper.writeValueAsString(courseList);
+      return ResponseEntity.ok().body(body);
+    }  
+  }
+
+  @GetMapping(value = "/api/member/courses/forInstructor/{email}", produces = "application/json")
+  public ResponseEntity<String> getMyCourses(@RequestHeader("Authorization") String authorization, @PathVariable("email") String email) throws JsonProcessingException {
+    if (!authControllerAdvice.getIsMember(authorization)){
+      return getUnauthorizedResponse("member");
+    }
+    List<Course> courseList = courseRepository.findAllByInstructorEmail(email);
+    ObjectMapper mapper = new ObjectMapper();
+    String body = mapper.writeValueAsString(courseList);
+    return ResponseEntity.ok().body(body);
+  }
+
+  @GetMapping(value = "/api/member/courses/{courseId}")
+  public ResponseEntity<String> showMemberCourse(@RequestHeader("Authorization") String authorization, @PathVariable("courseId") Long courseId) throws JsonProcessingException {
+    if (!authControllerAdvice.getIsMember(authorization)){
+      return getUnauthorizedResponse("member");
+    }
+    Optional<Course> course = courseRepository.findById(courseId);
+
+    if(course.isPresent()){
+      List<TutorAssignmentOfficeHourView> viewList = new ArrayList<>();
+      
+      List<TutorAssignment> tutorAssignments = tutorAssignmentRepository.findAllByCourse(course.get());
+      for(TutorAssignment temp : tutorAssignments){
+        List<OnlineOfficeHours> onlineOfficeHours = onlineOfficeHoursRespository.findAllByTutorAssignmentAndCourse(temp,course.get());
+
+        TutorAssignmentOfficeHourView tutorAssignmentOfficeHourView = new TutorAssignmentOfficeHourView(temp, onlineOfficeHours);
+        viewList.add(tutorAssignmentOfficeHourView);
+      }
+      
+      ObjectMapper mapper = new ObjectMapper();
+      String body = mapper.writeValueAsString(viewList);
+      return ResponseEntity.ok().body(body); 
+    }   
+    return ResponseEntity.notFound().build();
+    
+  }
+    
+  @GetMapping(value = "/api/public/courses/{courseId}")
+  public ResponseEntity<String> showCourse(@PathVariable("courseId") Long courseId) throws JsonProcessingException {
+    Optional<Course> course = courseRepository.findById(courseId);
+
+    if(course.isPresent()){
+      List<TutorAssignmentOfficeHourView> viewList = new ArrayList<>();
+      
+      List<TutorAssignment> tutorAssignments = tutorAssignmentRepository.findAllByCourse(course.get());
+      for(TutorAssignment temp : tutorAssignments){
+        List<OnlineOfficeHours> onlineOfficeHours = onlineOfficeHoursRespository.findAllByTutorAssignmentAndCourse(temp,course.get());
+        TutorAssignmentOfficeHourView tutorAssignmentOfficeHourView = new TutorAssignmentOfficeHourView(temp, onlineOfficeHours);
+        viewList.add(tutorAssignmentOfficeHourView);
+      }
+      
+      for(TutorAssignmentOfficeHourView temp: viewList){
+        List<OnlineOfficeHours> officeHourList = temp.getOnlineOfficeHours();
+        temp.getTutorAssignment().getTutor().setEmail(null);
+        for(OnlineOfficeHours tempo: officeHourList){
+          tempo.setZoomRoomLink(null);
+        }
+      }
+      ObjectMapper mapper = new ObjectMapper();
+      String body = mapper.writeValueAsString(viewList);
+      return ResponseEntity.ok().body(body); 
+    }   
+    return ResponseEntity.notFound().build();
+    }
+  
+
+    @GetMapping(value = "/api/public/tutors/{courseId}")
+    public ResponseEntity<String> showTutor(@PathVariable("courseId") Long courseId) throws JsonProcessingException {
+      Optional<Course> course = courseRepository.findById(courseId);
+  
+      if(course.isPresent()){
+        
+        List<TutorAssignment> tutorAssignments = tutorAssignmentRepository.findAllByCourse(course.get());               
+        List<Tutor> tutorList = new ArrayList<>();
+        for(TutorAssignment temp : tutorAssignments) {
+          tutorList.add(temp.getTutor());
+        }
+         
+        ObjectMapper mapper = new ObjectMapper();
+        String body = mapper.writeValueAsString(tutorList);
+        return ResponseEntity.ok().body(body); 
+      }   
+      return ResponseEntity.notFound().build();
+      }
+}
