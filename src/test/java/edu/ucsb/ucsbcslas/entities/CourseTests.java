0c7d4330faeb060625362c1b9a187a1c7405bba8
Alejandro Diera <alejandrodiera@ucsb.edu>
2020-12-09 17:29:36 -0800

    Moved CourseTest.java to entities folder :

--- src/test/java/edu/ucsb/ucsbcslas/entities/CourseTests.java
+++ src/test/java/edu/ucsb/ucsbcslas/entities/CourseTests.java
@@ -1,4 +1,4 @@
-package edu.ucsb.ucsbcslas.models;
+package edu.ucsb.ucsbcslas.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
