a838156778fa3968fd18eac208891acda5836aa2
Callter <quansen@ucsb.edu>
2020-12-10 21:08:13 +0800

    qw - modify tests

--- javascript/src/main/components/Courses/CourseDetail.js
+++ javascript/src/main/components/Courses/CourseDetail.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 
-const CourseDetail = ({courseId}) => {
+const CourseDetail = () => {
     return (
       <div>
-        <p>This is the course detail page {courseId}.</p>
+        <p>This is the course detail page.</p>
       </div>
     );
 }
--- javascript/src/test/components/Courses/CourseDetail.test.js
+++ javascript/src/test/components/Courses/CourseDetail.test.js
@@ -1,12 +1,6 @@
 import React from "react";
 import { waitFor, render } from "@testing-library/react";
 import CourseDetail from "main/components/Courses/CourseDetail";
-import { useParams} from "react-router-dom";
-
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"), // use real functions 
-  useParams: jest.fn(), // except for just this one
-}));
 
 describe("Course Detail Page Test", () => { 
 	test("renders without crashing", () => {
