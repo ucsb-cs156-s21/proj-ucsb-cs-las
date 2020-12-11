300fd89b7f9a96e452582485df0a16e1f063b3da
Callter <quansen@ucsb.edu>
2020-12-11 17:59:18 +0800

    qw - fixing features

--- javascript/src/main/pages/Courses/CourseShow.js
+++ javascript/src/main/pages/Courses/CourseShow.js
@@ -1,39 +1,39 @@
 import React from "react";
 import useSWR from "swr";
-import { fetchWithToken,fetchWithoutToken } from "main/utils/fetch";
+import { fetchWithToken, fetchWithoutToken } from "main/utils/fetch";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Loading from "main/components/Loading/Loading";
 import CourseDetail from "main/components/Courses/CourseDetail";
 
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-function CourseShow(){
-    const history = useHistory();
-    const {courseId} = useParams();
-    const { getAccessTokenSilently: getToken } = useAuth0();
-    const { data: roleInfo } = useSWR(
+function CourseShow() {
+  const history = useHistory();
+  const { courseId } = useParams();
+  const { getAccessTokenSilently: getToken } = useAuth0();
+  const { data: roleInfo } = useSWR(
     ["/api/myRole", getToken],
     fetchWithToken
-    );
-    let args = [`/api/public/courses/show/${courseId}`, fetchWithoutToken];
-    if (roleInfo) {
-      args = roleInfo.role === "Member" || roleInfo.role === "Admin" ? [[`/api/member/courses/show/${courseId}`, getToken], fetchWithToken] 
-        : [`/api/public/courses/show/${courseId}`, fetchWithoutToken];
+  );
+  let args = [`/api/public/courses/show/${courseId}`, fetchWithoutToken];
+  if (roleInfo) {
+    args = roleInfo.role === "Member" || roleInfo.role === "Admin" ? [[`/api/member/courses/show/${courseId}`, getToken], fetchWithToken]
+      : [`/api/public/courses/show/${courseId}`, fetchWithoutToken];
 
-    }
-    const { data: viewList, error} = useSWR(args);
-    if (error) {
-        return (
-          <h1>We encountered an error; please reload the page and try again.</h1>
-        );
-      }
-    if (!viewList) {
-        return <Loading />;
-    }
-    return(
-        <CourseDetail member = {roleInfo.role === "Member" || roleInfo.role === "Admin"} viewList={viewList}/>
-    )
+  }
+  const { data: viewList, error } = useSWR(...args);
+  if (error) {
+    return (
+      <h1>We encountered an error; please reload the page and try again.</h1>
+    );
+  }
+  if (!viewList) {
+    return <Loading />;
+  }
+  return (
+    <CourseDetail member={roleInfo ? roleInfo.role === "Member" || roleInfo.role === "Admin" : false} viewList={viewList} />
+  )
 }
 
 export default CourseShow;
