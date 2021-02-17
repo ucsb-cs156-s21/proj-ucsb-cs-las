bcb4e1aec229fa757a65777314e48559850fd332
Bryan Zheng <zhiren@cs.ucsb.edu>
2021-02-16 18:38:55 -0800

    zz - updated footer with better description

--- javascript/src/main/components/Footer/AppFooter.js
+++ javascript/src/main/components/Footer/AppFooter.js
@@ -3,10 +3,17 @@ import React from "react";
 const AppFooter = () => {
   return (
     <footer className="bg-light p-3 text-center">
-      This is a demo of a minimal React frontend + Spring Boot backend app.
-      Check out the source code on{" "}
-      <a href="https://github.com/ucsb-cs156-f20/demo-spring-nextjs-app">
-        GitHub
+      This app is a class project of {" "}
+      <a href="https://ucsb-cs156.github.io">
+        CMPSC 156
+      </a>
+       at {" "}
+      <a href="https://ucsb.edu">
+        UCSB
+      </a>
+      . Check out the source code {" "}
+      <a href="https://github.com/ucsb-cs156-w21/proj-ucsb-cs-las">
+        on GitHub
       </a>
       !
     </footer>
