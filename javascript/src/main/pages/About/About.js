103af977c9520aa2ebece690c215a2f5d339f130
Justin Surmani <justinsurmani@ucsb.edu>
2021-03-01 13:02:29 -0800

    JS - Changed the About page to include a description of the CS LAs app

--- javascript/src/main/pages/About/About.js
+++ javascript/src/main/pages/About/About.js
@@ -4,26 +4,39 @@ import { Jumbotron } from "react-bootstrap";
 const About = () => {
   return (
     <Jumbotron>
-      <h1>This is 127.0.0.1 (aka home)</h1>
+      <h1>About Us</h1>
       <div className="text-left">
-        <p>Welcome to UCSB CS LAs!</p>
+        <h2>Purpose</h2>
         <p>
-          This app can hopefully help you understand how to use React with
-          Spring in order to create a functional web application. This app is
-          primarily built using the following:
+          This app was designed as a way to manage the assignment of Computer Science LAs and TAs to available slots during open lab hours.
+          In-person lab hours and office hours have been cancelled due to the ongoing pandemic and will take place on zoom until further notice.
         </p>
-        <ul>
-          <li>
-            <a href="https://github.com/facebook/create-react-app">
-              Create React App
-            </a>{" "}
-            - used for creating the React frontend from scratch
-          </li>
-          <li>
-            <a href="https://start.spring.io/">Spring Boot Initializer</a> -
-            used for creating the Spring Boot backend
-          </li>
-        </ul>
+        <h3>Features</h3>
+        <li>
+          Users can login to the app with their @ucsb.edu gmail account
+        </li>
+        <li>
+          Instructors have access to update information about their courses
+        </li>
+        <li>
+           LAs and TAs have access to update their own office hours
+        </li>
+        <li>
+          Students may view available lab hours as well as office hours for each CS course
+        </li>
+        <li>
+          Maintains database of all current and past students that have served as LAs 
+        </li>
+        <p></p>
+        <h4>Team</h4>
+        <p>
+          This app was designed as a project course for CS156 students to learn
+          how to use React with Spring in order to create a functional web application.
+          Check out the source code here on {" "}
+            <a href="https://github.com/ucsb-cs156-w21/proj-ucsb-cs-las">
+              Github
+            </a>{"."}
+        </p>
       </div>
     </Jumbotron>
   );
