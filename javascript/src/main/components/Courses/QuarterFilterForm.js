25e67d477e9ce0fc59a893fcefaf7386a5add16c
Peter Bayerle <peterbayerle2@gmail.com>
2021-03-01 11:28:14 -0800

    pb: 3 todos on front end section of issue #121 complete

--- javascript/src/main/components/Courses/QuarterFilterForm.js
+++ javascript/src/main/components/Courses/QuarterFilterForm.js
@@ -1,21 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
+import { checkCourseQuarter } from "main/utils/CourseFormHelpers";
 
 const QuarterFilterForm = ({ upsertFilter }) => {
     const [filterVal, setFilterVal] = useState("");
+    const quarterRef = useRef(null);
+
     const handleOnSubmit = (e) => {
         e.preventDefault();
-        upsertFilter(filterVal);
+        var isValid = checkCourseQuarter(filterVal);
+        if (isValid) {
+            upsertFilter(filterVal);
+            quarterRef.current.classList.add('is-valid');
+            quarterRef.current.classList.remove('is-invalid');
+        } else {
+            quarterRef.current.classList.remove('is-valid');
+            quarterRef.current.classList.add('is-invalid');        }
     }
+
     return (
         <Form onSubmit={handleOnSubmit}>
             <Form.Group as={Row} controlId="name">
                 <Form.Label column sm={2}>Enter quarter to filter by</Form.Label>
                 <Col sm={10}>
-                    <Form.Control type="text" placeholder="active quarter to filter by, enter blank field to have no filter" onChange={
+                    <Form.Control ref={quarterRef} type="text" placeholder="Ex: W21" onChange={
                         (e) => setFilterVal(e.target.value)
                     }
                     />
+                    <Form.Control.Feedback style={{ textAlign: "left" }} type="invalid">
+                        Please provide a valid quarter.
+                    </Form.Control.Feedback>
+                    <Form.Text style={{ textAlign: "left" }} muted>Use the first name of the quarter with the last 2 digits of the year. Ex (2020): F20, W20, S20, M20</Form.Text>
 
                 </Col>
             </Form.Group>
