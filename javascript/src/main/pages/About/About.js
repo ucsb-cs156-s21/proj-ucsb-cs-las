f0ef4998c6e4149ff67b1b973fbc78d15404b425
billy <zhengwei@ucsb.edu>
2021-03-01 13:46:33 -0800

    bw - change page formatting and inner tags

--- javascript/src/main/pages/About/About.js
+++ javascript/src/main/pages/About/About.js
@@ -7,29 +7,35 @@ const About = () => {
       <h1>About Us</h1>
       <div className="text-left">
         <h2>Purpose</h2>
+		<hr/>
         <p>
           This app was designed as a way to manage the assignment of Computer Science LAs and TAs to available slots during open lab hours.
           In-person lab hours and office hours have been cancelled due to the ongoing pandemic and will take place on zoom until further notice.
         </p>
-        <h3>Features</h3>
-        <li>
-          Users can login to the app with their @ucsb.edu gmail account
-        </li>
-        <li>
-          Instructors have access to update information about their courses
-        </li>
-        <li>
-           LAs and TAs have access to update their own office hours
-        </li>
-        <li>
-          Students may view available lab hours as well as office hours for each CS course
-        </li>
-        <li>
-          Maintains database of all current and past students that have served as LAs 
-        </li>
-        <p></p>
-        <h4>Team</h4>
-        <p>
+		
+        <h2>Features</h2>
+        <hr/>
+		<ul>
+		  <li>
+		    Users can login to the app with their @ucsb.edu gmail account
+	  	  </li>
+		  <li>
+		    Instructors have access to update information about their courses
+		  </li>
+		  <li>
+		     LAs and TAs have access to update their own office hours
+		  </li>
+		  <li>
+		    Students may view available lab hours as well as office hours for each CS course
+		  </li>
+		  <li>
+		    Maintains database of all current and past students that have served as LAs 
+		  </li>
+		</ul>
+		
+        <h2>Team</h2>
+        <hr/>
+		<p>
           This app was designed as a project course for CS156 students to learn
           how to use React with Spring in order to create a functional web application.
           Check out the source code here on {" "}
