28446532875e8ba0ce932d931178992d8a9c7b54
Leo <lvaldivia.jr@gmail.com>
2021-03-07 19:25:27 -0800

    lv - Add CSVToObject service in backend

---
+++ src/main/java/edu/ucsb/ucsbcslas/services/CSVToObjectServiceImpl.java
@@ -0,0 +1,29 @@
+package edu.ucsb.ucsbcslas.services;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.Reader;
+import java.util.List;
+import java.lang.Class;
+
+@Service
+public class CSVToObjectServiceImpl<T> implements CSVToObjectService<T> {
+    private static Logger log = LoggerFactory.getLogger(CSVToObjectServiceImpl.class);
+
+    @Override
+    public List<T> parse(Reader csv, Class<T> type) {
+        return new CsvToBeanBuilder<T>(csv)
+            .withSkipLines(1)
+            .withType(type)
+            .build()
+            .parse();
+    }
+
+    @Override
+    public Logger getLogger() {
+        return log;
+    }
+} 
