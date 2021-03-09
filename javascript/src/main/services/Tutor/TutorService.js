7421f2d344967254d76d71bab4c5b56cba24c6e7
leo <lvaldivia.jr@gmail.com>
2021-03-08 23:40:11 -0800

    lv - fix api call error

--- javascript/src/main/services/Tutor/TutorService.js
+++ javascript/src/main/services/Tutor/TutorService.js
@@ -77,7 +77,7 @@ const uploadTutorsCSV = (getToken, onSuccess, onError) => {
     const data = new FormData(); 
     data.append("csv", file); 
     try {
-      await fetchWithToken('/api/sutdents/upload', getToken, {
+      await fetchWithToken('/api/member/tutors/', getToken, {
         method: "POST", 
         body: data
       });
