d7a8f958ae2024ee5faf764fc7632614dc40d721
leo <lvaldivia.jr@gmail.com>
2021-03-09 00:39:13 -0800

    Change tutor table permissions

--- javascript/src/main/pages/Tutor/Tutor.js
+++ javascript/src/main/pages/Tutor/Tutor.js
@@ -67,24 +67,30 @@ const Tutor = () => {
   const uploadTutors = uploadTutorsCSV(getToken, mutateTutors); 
 
   return (
-    <>
-      <Button
-        data-testid={`new-tutor-button`}
-        onClick={() => history.push("/tutors/new")}
-      >
-        New Tutor
-      </Button>
-      <TutorCSVButton admin={isAdmin} addTask={uploadTutors} />
-      <pre style={{whiteSpace: 'pre', textAlign: 'left', width: 'auto', marginLeft: 'auto', marginRight: 'auto', padding: '0em'}} muted>
-      Required Columns: firstName, lastName, email.  Ex: joe, gaucho, joegaucho@ucsb.edu
-      </pre>
 
-      <TutorTable
-        tutors={tutorList}
-        instructorTutors={instructorTutorList}
-        admin={isAdmin}
-        deleteTutor={deleteTutor}
+    <>
+         {(isInstructor || isAdmin) && (
+        <><Button
+          data-testid={`new-tutor-button`}
+          onClick={() => history.push("/tutors/new")}
+        >
+          New Tutor
+        </Button>
+        <TutorCSVButton admin={isAdmin} addTask={uploadTutors} />
+          <pre style={{ whiteSpace: 'pre', textAlign: 'left', width: 'auto', marginLeft: 'auto', marginRight: 'auto', padding: '0em' }} muted>
+            Required Columns: firstName, lastName, email.Ex: joe, gaucho, joegaucho @ucsb.edu
+          </pre></>
+         )}
+
+        {instructorTutorList && (
+
+        <TutorTable
+          tutors={tutorList}
+          instructorTutors={instructorTutorList}
+          admin={isAdmin || isInstructor}
+          deleteTutor={deleteTutor}
         />
+        )}
     </>
   );
 };
