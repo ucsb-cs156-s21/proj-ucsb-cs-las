c6e00abffef28098a4c72e6489cc7ebb7627c668
leo <lvaldivia.jr@gmail.com>
2021-03-09 17:01:58 -0800

    lv - add toasts to uploadCSV

--- javascript/src/main/pages/Tutor/Tutor.js
+++ javascript/src/main/pages/Tutor/Tutor.js
@@ -7,7 +7,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Loading from "main/components/Loading/Loading";
 import TutorTable from "main/components/Tutor/TutorTable";
 import { TutorCSVButton } from "./TutorCSVButton"; 
-//import { useToasts } from "react-toast-notifications";
+import { useToasts } from "react-toast-notifications";
 
 import {
   //buildCreateTutor,
@@ -23,6 +23,7 @@ const Tutor = () => {
   const { email } = user;
   const history = useHistory();
   const { data: roleInfo } = useSWR(["/api/myRole", getToken], fetchWithToken);
+  const {addToast} = useToasts();
 
   const { data: tutorList, error, mutate: mutateTutors } = useSWR(
     ["/api/member/tutors", getToken],
@@ -64,7 +65,17 @@ const Tutor = () => {
 
   const deleteTutor = buildDeleteTutor(getToken, mutateTutors);
 
-  const uploadTutors = uploadTutorsCSV(getToken, mutateTutors); 
+  //const uploadTutors = uploadTutorsCSV(getToken, mutateTutors); 
+   
+    const uploadTutors = uploadTutorsCSV(getToken,
+    () => {
+      mutateTutors(); 
+      addToast("CSV Uploaded", { appearance: "success" });
+    },
+    () => {
+      addToast("Error Uploading CSV", { appearance: "error" });
+    }
+  );
 
   return (
     <>
