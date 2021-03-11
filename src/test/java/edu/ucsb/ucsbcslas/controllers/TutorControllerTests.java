cb2f4e27f0daecc886e0a262aa2bc7800a93ad8c
Leonardo <lvaldivia.jr@gmail.com>
2021-03-11 13:43:37 -0800

    add mock import

--- src/test/java/edu/ucsb/ucsbcslas/controllers/TutorControllerTests.java
+++ src/test/java/edu/ucsb/ucsbcslas/controllers/TutorControllerTests.java
@@ -5,6 +5,7 @@ import org.junit.Ignore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.Mock
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
