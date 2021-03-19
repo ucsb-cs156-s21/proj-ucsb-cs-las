b418afb49a1097ea6b1d1cb624770d15faef376c
Bryan Terce <bryan.terce@gmail.com>
2021-03-19 15:14:42 -0700

    Create tutor assignment select component

---
+++ javascript/src/main/components/TutorAssignment/TutorAssignmentSelect.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import { Cascader } from "antd";
+import { fetchWithoutToken } from "main/utils/fetch";
+import { asHumanQuarter } from "main/utils/quarter";
+
+const defaultQuarterProvider = () => fetchWithoutToken("/api/public/quarters");
+const defaultCourseProvider = quarter => fetchWithoutToken(`/api/public/courses/forQuarter/${quarter}`);
+const defaultTutorAssignmentProvider = course => fetchWithoutToken(`api/public/tutorAssignment/${course}`);
+
+export default function TutorAssignmentSelect(props) {
+  const {
+    quarterProvider = defaultQuarterProvider,
+    courseProvider = defaultCourseProvider,
+    tutorAssignmentProvider = defaultTutorAssignmentProvider,
+    onChange,
+    ...otherProps
+  } = props;
+  const [options, setOptions] = useState(null);
+
+  const onCascaderChange = (value) => {
+    if (onChange) {
+      onChange(value[2]);
+    }
+  };
+
+  useEffect(() => {
+    quarterProvider()
+      .then(quarters => {
+        setOptions(quarters.map(q => ({
+          value: q,
+          label: asHumanQuarter(q),
+          isLeaf: false
+        })));
+      })
+  }, [quarterProvider]);
+
+  const loadData = async selectedOptions => {
+    if (selectedOptions.length === 1) {
+      const selectedQuarter = selectedOptions[0];
+      selectedQuarter.loading = true;
+
+      const courses = await courseProvider(selectedQuarter.value);
+
+      selectedQuarter.loading = false;
+      selectedQuarter.children =
+        courses.map(c => ({
+          value: c.id,
+          label: c.name,
+          isLeaf: false
+        }));
+    } else {
+      const selectedCourse = selectedOptions[1];
+      selectedCourse.loading = true;
+
+      const tutorAssignments = await tutorAssignmentProvider(selectedCourse.value);
+
+      selectedCourse.loading = false;
+      selectedCourse.children =
+        tutorAssignments.map(ta => ({
+          value: ta.id,
+          label: `${ta.tutor.firstName} ${ta.tutor.lastName}`
+        }));
+    }
+
+    setOptions([...options]); // force rerender
+  }
+
+  return (
+    <Cascader
+      {...otherProps}
+      options={options}
+      loadData={loadData}
+      onChange={onCascaderChange}
+      placeholder={options ? "Select a tutor" : "Loading..."}
+      disabled={!options}
+    />
+  );
+}
---
+++ javascript/src/test/components/TutorAssignment/TutorAssignmentSelect.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { render, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import TutorAssignmentSelect from "main/components/TutorAssignment/TutorAssignmentSelect";
+
+describe("TutorAssignmentSelect", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("renders a usable tutor assignment UI", async () => {
+    const onChange = jest.fn();
+    const quarterProvider = jest.fn().mockResolvedValue(["20201"]);
+    const courseProvider = jest.fn().mockResolvedValue([{ id: 1, name: "CMPSC 156" }]);
+    const tutorAssignmentProvider = jest.fn().mockResolvedValue([{ id: 2, tutor: { firstName: "Foo", lastName: "Bar" }}]);
+    const { debug, getByPlaceholderText, getByText } = render(
+      <TutorAssignmentSelect
+        onChange={onChange}
+        quarterProvider={quarterProvider}
+        courseProvider={courseProvider}
+        tutorAssignmentProvider={tutorAssignmentProvider}
+      />
+    );
+
+    await waitFor(() => expect(quarterProvider).toHaveBeenCalledTimes(1));
+
+    userEvent.click(getByPlaceholderText("Select a tutor"));
+    userEvent.click(getByText("Winter 2020"));
+
+    await waitFor(() => expect(courseProvider).toHaveBeenCalledWith("20201"));
+
+    userEvent.click(getByText("CMPSC 156"));
+
+    await waitFor(() => expect(tutorAssignmentProvider).toHaveBeenCalledWith(1));
+
+    userEvent.click(getByText("Foo Bar"));
+
+    expect(onChange).toHaveBeenCalledWith(2);
+  });
+});
+
+
