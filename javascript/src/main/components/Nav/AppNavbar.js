1763c5de7199c106ec294db6e2959674541c7f8f
Black Hat <bhat@encom.eu.org>
2021-05-11 23:19:28 -0700

    hl - fix absolute path for navbar logo

--- javascript/src/main/components/Nav/AppNavbar.js
+++ javascript/src/main/components/Nav/AppNavbar.js
@@ -18,7 +18,7 @@ function AppNavbar() {
     <Navbar bg="dark" variant="dark">
       <LinkContainer to={""}>
         <Navbar.Brand data-testid="brand">
-        <p className="brand"><img className="brand" src={'newLogo.png'} alt = "NEED TO BE FIXED"/></p>
+        <p className="brand"><img className="brand" src={'%PUBLIC_URL%/newLogo.png'} alt = "UCSB CS LAS Logo"/></p>
         <p className="brand">UCSB <br />CS LAs</p>
         </Navbar.Brand>
       </LinkContainer>
