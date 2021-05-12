c92ad30d97b336ae7477aaecd0e6fa0bc24c5fef
bzamora020 <bryan.aldair.zamora@gmail.com>
2021-05-12 13:32:09 -0700

    BZ - Added credits for favicon and logo on footer

--- javascript/src/main/components/Footer/AppFooter.js
+++ javascript/src/main/components/Footer/AppFooter.js
@@ -3,18 +3,23 @@ import React from "react";
 const AppFooter = () => {
   return (
     <footer className="bg-light p-3 text-center">
-      This app is a class project of {" "}
-      <a href="https://ucsb-cs156.github.io">
-        CMPSC 156
-      </a>
-        &nbsp;at {" "}
-      <a href="https://ucsb.edu">
-        UCSB
-      </a>
-      . Check out the source code on {" "}
-      <a href="https://github.com/ucsb-cs156-w21/proj-ucsb-cs-las">
-        GitHub
-      </a>
+      This app is a class project of{" "}
+      <a href="https://ucsb-cs156.github.io">CMPSC 156</a>
+      &nbsp;at <a href="https://ucsb.edu">UCSB</a>. Check out the source code on{" "}
+      <a href="https://github.com/ucsb-cs156-w21/proj-ucsb-cs-las">GitHub</a>
+      <p>
+        The cartoon Storke Tower images in the brand logo and favicon for this
+        site were developed by Chelsea Lyon-Hayden, Art Director for UCSB
+        Associate Students, and are used here by permission of the Executive
+        Director of UCSB Associated Students. These images are Copyright © 2021
+        UCSB Associated Students, and may not be reused without express written
+        permission of the Executive Director of UCSB Associated Students. For
+        more info, visit:
+        <br></br>
+        <a href="https://www.as.ucsb.edu/sticker-packs/>">
+          www.as.ucsb.edu/sticker-packs/
+        </a>
+      </p>
       !
     </footer>
   );
