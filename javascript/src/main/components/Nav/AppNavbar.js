96327880dd92271a28db06380a8388ec85be0d55
Sam <qliang@ucsb.edu>
2021-05-11 19:49:20 -0700

    changed AppNavbar.js and App.css to make logo shown on home page but not yet all pages

--- javascript/src/main/components/Nav/AppNavbar.js
+++ javascript/src/main/components/Nav/AppNavbar.js
@@ -17,7 +17,10 @@ function AppNavbar() {
   return (
     <Navbar bg="dark" variant="dark">
       <LinkContainer to={""}>
-        <Navbar.Brand data-testid="brand">UCSB CS LAs</Navbar.Brand>
+        <Navbar.Brand data-testid="brand">
+        <p className="brand"><img className="brand" src={'newLogo.png'} alt = "NEED TO BE FIXED"/></p>
+        <p className="brand">UCSB <br />CS LAs</p>
+        </Navbar.Brand>
       </LinkContainer>
       <Nav>
         { isAdmin &&
