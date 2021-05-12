3f19366cb0e0e6c4f5378a20d1a7295a0a8f0cf8
Zion Wang <zion@ucsb.umail.edu>
2021-05-12 13:20:27 -0700

    zw - an - fixed the code cov issue that we ran into

--- javascript/src/main/components/Courses/CourseTable.js
+++ javascript/src/main/components/Courses/CourseTable.js
@@ -32,21 +32,6 @@ export default ({ courses, admin, deleteCourse }) => {
         )
     }
 
-    // const sortCaret = (order, _column) => {
-    //     // const ascendingON = String.fromCharCode(0x25b2);
-    //     // const descendingON = String.fromCharCode(0x25bc);
-    //     // const ascendingOFF = String.fromCharCode(0x25b3);
-    //     // const descendingOFF = String.fromCharCode(0x25bd);
-
-    //     // if (!order) 
-    //     //     return (<span data-testid="sort">{descendingOFF}{ascendingOFF}</span>);
-    //     // else if (order === 'asc') 
-    //     //     return (<span data-testid="sort-asc">{descendingOFF}<font color="red">{ascendingON}</font></span>);
-    //     // else 
-    //     //     return (<span data-testid="sort-desc"><font color="red">{descendingON}</font>{ascendingOFF}</span>);
-        
-    // }
-
     function stripesFormatter(cell) {
 
         return (
@@ -71,36 +56,30 @@ export default ({ courses, admin, deleteCourse }) => {
         dataField: 'id',
         text: 'id',
         sort: true,
-        //sortCaret: sortCaret
     }, {
         dataField: 'name',
         text: 'Course Number',
         sort: true,
-        //sortCaret: sortCaret,
         formatter: (_cell, row) => courseLinkFormatter(row.id, row.name)
     }, {
         dataField: 'quarter',
         text: 'Quarter',
         sort: true,
-        //sortCaret: sortCaret
     }, {
         dataField: 'instructorFirstName',
         text: 'First',
         sort: true,
-        //sortCaret: sortCaret,
         formatter: stripesFormatter
 
     }, {
         dataField: 'instructorLastName',
         text: 'Last',
         sort: true,
-        //sortCaret: sortCaret,
         formatter: stripesFormatter
     }, {
         dataField: 'instructorEmail',
         text: 'Email',
         sort: true,
-        //sortCaret: sortCaret,
         formatter: stripesFormatter
     }];
 
--- javascript/src/test/components/Courses/CourseTable.test.js
+++ javascript/src/test/components/Courses/CourseTable.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import CourseTable from "main/components/Courses/CourseTable"
-//import userEvent from "@testing-library/user-event";
 
 
 describe("CourseForm tests", () => {
@@ -28,16 +27,5 @@ describe("CourseForm tests", () => {
   test("renders without crashing", () => {
     render(<CourseTable courses={courses} admin={true} deleteCourse={deleteCourse} />);
   });
-
-  // test("ascending and descending buttons can be pressed", () => {
-  //   const { getByText } = render(<CourseTable courses={courses} admin={true} deleteCourse={deleteCourse} />);
-  //   const courseNumberHeader = getByText(/Course Number/);
-
-  //   //cycle through both ascending and descending for test coverage
-  //   userEvent.click(courseNumberHeader);
-  //   userEvent.click(courseNumberHeader);
-  //   const descendingOFF = String.fromCharCode(0x25bd);
-  //   expect(getByText(descendingOFF)).toBeInTheDocument();
-  // });
 
 });
