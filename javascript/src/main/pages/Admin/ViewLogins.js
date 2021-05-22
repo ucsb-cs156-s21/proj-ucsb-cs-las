76b2e815dd52a19c8b37b4e6569dfdd48bbc41d0
Black Hat <bhat@encom.eu.org>
2021-05-22 01:18:45 -0700

    hl - add CSV button for downloading all logins

--- javascript/src/main/pages/Admin/ViewLogins.js
+++ javascript/src/main/pages/Admin/ViewLogins.js
@@ -3,7 +3,22 @@ import useSWR from "swr";
 import { useAuth0 } from "@auth0/auth0-react";
 import { fetchWithToken } from "main/utils/fetch";
 import LoginTable from "main/components/Admin/LoginTable"
+import { CSVLink } from "react-csv";
 
+
+const headers = [{
+  key: 'timestamp',
+  label: 'Timestamp'
+}, {
+  key: 'email',
+  label: 'Email'
+}, {
+  key: 'firstName',
+  label: 'First'
+}, {
+  key: 'lastName',
+  label: 'Last'
+}];
 
 const ViewLogins = () => {
   const { getAccessTokenSilently: getToken } = useAuth0();
@@ -13,6 +28,7 @@ const ViewLogins = () => {
     <>
       <h2 className="text-left">Login History</h2>
       <LoginTable loginTable={loginTable || []} admin={true}/>
+      <Button><CSVLink style={{color: "white"}} headers={headers} data={loginTable || []} filename = {"LoginTable.csv"}>Download CSV</CSVLink></Button>
     </>
   );
 };
