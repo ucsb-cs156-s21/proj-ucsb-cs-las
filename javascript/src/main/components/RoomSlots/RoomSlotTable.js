59c9fb143f0e64437f8e649bf01d496872201139
Rowan-James Tran <rowanjames.tran@gmail.com>
2021-05-25 21:16:06 -0700

    rc/rt - add table for room slots, include the table on the room slots page with empty placeholder data

--- javascript/src/main/components/RoomSlots/RoomSlotTable.js
+++ javascript/src/main/components/RoomSlots/RoomSlotTable.js
@@ -1,7 +1,53 @@
+
 import React from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
 import { Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
-export default({}) => {
-    
-};
+/**
+ * Displays the list of room slots. Code adapted from TutorTable.
+ */
+const RoomSlotTable = ({ roomSlots, admin, deleteRoomSlot }) => {
+    const history = useHistory();
+
+    const AdminRenderWrapper = ({children }) => {
+        return (admin && children);
+    };
+
+    const columns = [
+        {
+            dataField: "location",
+            text: "Location",
+            sort: true,
+        },
+        {
+            dataField: "quarter",
+            text: "Quarter",
+            sort: true,
+        },
+        {
+            dataField: "dayOfWeek",
+            text: "Day of Week",
+            sort: true,
+        },
+        {
+            dataField: "startTime",
+            text: "Start Time",
+            sort: true,
+        },
+        {
+            dataField: "endTime",
+            text: "End Time",
+            sort: true,
+        }
+    ];
+
+    return <BootstrapTable
+        bootstrap4={true}
+        keyField="id"
+        data={roomSlots}
+        columns={columns}
+    />;
+}
+
+export default RoomSlotTable;
--- javascript/src/main/pages/RoomSlots/RoomSlots.js
+++ javascript/src/main/pages/RoomSlots/RoomSlots.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { Button } from "react-bootstrap";
+
+import RoomSlotTable from "main/components/RoomSlots/RoomSlotTable";
+
 const RoomSlots = () => {
   const history = useHistory();
   return (
     <>
       <h1>Room Slots</h1>
       <Button className="mb-3" onClick={()=>history.push("/roomslots/new")} >New Room Slot</Button>
+      <RoomSlotTable roomSlots={[]} admin={true} deleteRoomSlot={() => {}} />
     </>
   );
 }
