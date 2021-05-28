7a02ed2c3b18ce47cce2ae771bf9c308280333e9
Rowan-James Tran <rowanjames.tran@gmail.com>
2021-05-28 01:07:12 -0700

    rt - update ObjectMapperConfig to write dates as strings, update RoomSlotTable to format data nicely

--- javascript/src/main/components/RoomSlots/RoomSlotTable.js
+++ javascript/src/main/components/RoomSlots/RoomSlotTable.js
@@ -3,6 +3,8 @@ import React from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
 import { Button } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
+import fromFormat from "main/utils/FromFormat";
+import { toAMPMFormat } from "main/utils/RoomSlotTableHelpers";
 
 /**
  * Displays the list of room slots. Code adapted from TutorTable.
@@ -24,21 +26,27 @@ const RoomSlotTable = ({ roomSlots, admin, deleteRoomSlot }) => {
             dataField: "quarter",
             text: "Quarter",
             sort: true,
+            formatter: fromFormat
         },
         {
             dataField: "dayOfWeek",
             text: "Day of Week",
             sort: true,
+            formatter: (day) => {
+                return day.substring(0, 1) + day.substring(1).toLowerCase();
+            }
         },
         {
             dataField: "startTime",
             text: "Start Time",
             sort: true,
+            formatter: toAMPMFormat
         },
         {
             dataField: "endTime",
             text: "End Time",
             sort: true,
+            formatter: toAMPMFormat
         }
     ];
 
