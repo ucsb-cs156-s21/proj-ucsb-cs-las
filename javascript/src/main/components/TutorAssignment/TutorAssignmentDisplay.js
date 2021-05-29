7cd380f8a2ec62fbf19c6e2dcc0869cf0d074d98
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-05-29 16:04:20 -0700

    pc/kw/cx - make the TutorAssignmentDisplay have nicer looking output

--- javascript/src/main/components/TutorAssignment/TutorAssignmentDisplay.js
+++ javascript/src/main/components/TutorAssignment/TutorAssignmentDisplay.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { FormControl,Row,Col } from "react-bootstrap";
+import {asHumanQuarter} from "main/utils/quarter.ts"
+
 
 export default function TutorAssignmentDisplay({tutorAssignment}) {
     console.log("tutorAssignment=",tutorAssignment)
     return (
-        <>
-        <div>{tutorAssignment.tutor == null ? "" : tutorAssignment.tutor.firstName} {tutorAssignment.tutor == null ? "" : tutorAssignment.tutor.lastName} {tutorAssignment.course == null ? "" : tutorAssignment.course.name} {tutorAssignment.course == null ? "" : tutorAssignment.course.quarter}</div>
-        </>
-    )
+        <Row>
+            <Col>
+                <FormControl readOnly type="text" value={tutorAssignment.tutor == null ? "" : tutorAssignment.tutor.firstName + ' ' + tutorAssignment.tutor.lastName }/>
+            </Col>
+            <Col>
+                <FormControl readOnly type="text" value={ tutorAssignment.course == null ? "" : tutorAssignment.course.name }/>
+            </Col>
+            <Col>
+                <FormControl readOnly type="text" value={ tutorAssignment.course == null ? "" : asHumanQuarter(tutorAssignment.course.quarter) }/>
+            </Col>
+        </Row>
+    );
 }
---
+++ javascript/src/stories/components/TutorAssignment/TutorAssignmentDisplay.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import TutorAssignmentDisplay from "main/components/TutorAssignment/TutorAssignmentDisplay";
+import tutorAssignmentFixtures from '../../../fixtures/tutorAssignmentFixtures';
+
+export default {
+  title: "components/TutorAssignment/TutorAssignmentDisplay",
+  component: TutorAssignmentDisplay,
+  argTypes: {
+    onChange: { action: "changed" }
+  }
+};
+
+const Template = (args) => <TutorAssignmentDisplay {...args} />;
+
+export const Scott_Chow_CMPSC_156_S20 = Template.bind({});
+Scott_Chow_CMPSC_156_S20.args = {
+    tutorAssignment: tutorAssignmentFixtures.Scott_Chow_CMPSC_156_S20
+};
+
+export const Alex_Gerber_PSTAT_120A_M20 = Template.bind({});
+Alex_Gerber_PSTAT_120A_M20.args = {
+    tutorAssignment: tutorAssignmentFixtures. Alex_Gerber_PSTAT_120A_M20
+};
