bed8fc480b33e2aa321547050954a574fe226418
Zion Wang <zion@ucsb.umail.edu>
2021-05-29 11:12:53 -0700

    edited the tutor notes forum so that it works but can't push to the database yet understandably still need to wait to test the table, also need to figure out these test

--- javascript/src/main/components/TutorNotes/TutorNotesForm.js
+++ javascript/src/main/components/TutorNotes/TutorNotesForm.js
@@ -11,7 +11,7 @@ const TutorNotesForm = ({ createTutorNotes, updateTutorNotes, existingTutorNotes
         id: null,
         course: null,
         tutor: null,
-        message: "Random Message"
+        message: "Write message here"
     }
 
     const [existingSet, setExistingSet] = useState(false);
@@ -20,33 +20,44 @@ const TutorNotesForm = ({ createTutorNotes, updateTutorNotes, existingTutorNotes
     if(existingTutorNotes && !existingSet){
         setTutorNotes({course: existingTutorNotes.course, 
             tutor: existingTutorNotes.tutor,
-            tutorEmail: existingTutorNotes.tutor.email, 
             message: existingTutorNotes.message, 
             id: existingTutorNotes.id});
         setExistingSet(true);
     }
 
     const { getAccessTokenSilently: getToken } = useAuth0();
+
     const { data: courseList, error } = useSWR(
         ["/api/member/courses/", getToken],
         fetchWithToken
     );
 
-    let sortedList;
-    if (error) {
+    const { data: tutorList, error2 } = useSWR(
+        ["/api/member/tutors/", getToken],
+        fetchWithToken
+    );
+
+
+    let sortedListc;
+    let sortedListt;
+    if (error || error2) {
         return (
             <>
-            <h1>You must be an instructor or an admin to create new Tutor Notes.</h1>
+            <h1>You must be an instructor or an admin to create new Tutor Notes or we have found no Courses/Tutors</h1>
             </>
         );
     }
-    if (!courseList) {
+    if (!courseList || !tutorList) {
         return <Loading />;
     }
     else {
-        sortedList = courseList.map((course, index) => <option key={index} value={index}>{course.name+" "+asHumanQuarter(course.quarter)}</option>);
+        sortedListc = courseList.map((course, index) => <option key={index} value={index}>{course.name+" "+asHumanQuarter(course.quarter)}</option>);
         if(tutorNotes.course === null){
             setTutorNotes({...tutorNotes, course: courseList[0], index: 0});
+        }
+        sortedListt = tutorList.map((tutor, index) => <option key={index} value={index}>{tutor.firstName+" "+tutor.lastName}</option>);
+        if(tutorNotes.tutor === null){
+            setTutorNotes({...tutorNotes, tutor: tutorList[0]});
         }
     }
 
@@ -54,6 +65,11 @@ const TutorNotesForm = ({ createTutorNotes, updateTutorNotes, existingTutorNotes
     if(existingTutorNotes && tutorNotes.index === null){
         for (var i = 0; i < courseList.length; i++) {
             if (courseList[i].id === existingTutorNotes.course.id) {
+                setTutorNotes({...tutorNotes, index: i});
+            }
+        }
+        for (var i = 0; i < tutorList.length; i++) {
+            if (tutorList[i].id === existingTutorNotes.tutor.id) {
                 setTutorNotes({...tutorNotes, index: i});
             }
         }
@@ -77,28 +93,27 @@ const TutorNotesForm = ({ createTutorNotes, updateTutorNotes, existingTutorNotes
                     <Form.Label column sm={2}>Course Name</Form.Label>
                     <Col sm={10}>
                         <Form.Control as="select" value={tutorNotes.index} onChange={(e) => setTutorNotes({ 
-                            ...tutorNotes, course: courseList[e.target.value], index: e.target.value})}>
-                            {sortedList}
+                            ...tutorNotes, course: courseList[e.target.value]})}>
+                            {sortedListc}
                         </Form.Control>
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row} controlId="tutorEmail">
                     <Form.Label column sm={2}>
-                        Tutor Email
+                        Tutor
                     </Form.Label>
                     <Col sm={10}>
-                        <Form.Control type="text" placeholder="tutor email" value={tutorNotes.tutor} onChange={(e) => setTutorNotes({
-                            ...tutorNotes,
-                            tutor: e.target.value
-                        })} />
+                        <Form.Control as="select" value={tutorNotes.index} onChange={(e) => setTutorNotes({ 
+                            ...tutorNotes, tutor: tutorList[e.target.value]})}>
+                            {sortedListt}
+                        </Form.Control>
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row} controlId="message">
                     <Form.Label column sm={2}>Message</Form.Label>
                     <Col sm={10}>
-                        <Form.Control as="text" value={tutorNotes.message} onChange={(e) => setTutorNotes({ 
-                            ...tutorNotes, message: e.target.value})}>
-                        </Form.Control>
+                        <Form.Control type="text" placeholder="Write note here" onChange={(e) => setTutorNotes({ 
+                            ...tutorNotes, message: e.target.value})}/>
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row}>
