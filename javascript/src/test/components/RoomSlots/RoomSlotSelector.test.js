4ca13a212751bc04f7193524f9fd7cd46b6ef9d9
Rowan-James Tran <rowanjames.tran@gmail.com>
2021-06-02 12:19:47 -0700

    rt - work on RoomSlotSelector and associated files

--- javascript/src/test/components/RoomSlots/RoomSlotSelector.test.js
+++ javascript/src/test/components/RoomSlots/RoomSlotSelector.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import RoomSlotSelector from "main/components/RoomSlots/RoomSlotSelector";
+import roomSlotFixtures from "fixtures/roomSlotFixtures";
 
 import { asHumanQuarter } from "main/utils/quarter";
 jest.mock("main/utils/quarter");
@@ -10,25 +11,6 @@ import { toAMPMFormat } from "main/utils/RoomSlotTableHelpers";
 jest.mock("main/utils/RoomSlotTableHelpers");
 
 describe("RoomSlotSelector tests", () => {
-    const slots = [
-        {
-            id: 1,
-            location: "Library",
-            quarter: "S21",
-            dayOfWeek: "MONDAY",
-            startTime: "08:00:00",
-            endTime: "15:00:00",
-        },
-        {
-            id: 2,
-            location: "HSSB",
-            quarter: "S21",
-            dayOfWeek: "TUESDAY",
-            startTime: "08:00:00",
-            endTime: "15:00:00",
-        },
-    ]
-
     beforeEach(() => {
         jest.clearAllMocks();
         asHumanQuarter.mockImplementation((quarter) => {
